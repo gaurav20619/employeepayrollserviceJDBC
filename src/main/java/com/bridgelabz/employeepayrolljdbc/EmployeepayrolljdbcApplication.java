f4b16e7493e31b2790d2e2f0a752b770b69950dd
gaurav20619 <gauravsinghraijada619@gmail.com>
2023-03-20 06:47:39 +0530

    Add: first commit

---
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeepayrolljdbcApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrolljdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeepayrolljdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeepayrolljdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/employeepayrolljdbc/EmployeepayrolljdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrolljdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeepayrolljdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
